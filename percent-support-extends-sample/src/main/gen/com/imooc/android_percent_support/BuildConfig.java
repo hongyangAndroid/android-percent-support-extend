e400e72b45d01b52e2dd37e8dd390c1baf731328
hongyangAndroid <lmj623565791@gmail.com>
2015-09-30 17:21:44 +0800

    调整结构

---
+++ percent-support-extends-sample/src/main/gen/com/imooc/android_percent_support/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package com.imooc.android_percent_support;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
